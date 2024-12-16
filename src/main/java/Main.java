ab2ed69e3e004804f45d1201c8c3223a1dd4dea5
kate_u <kate.udachina@gmail.com>
2024-12-16 22:21:54 +0300

    Домашняя работа

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,10 @@
-
 import java.util.Scanner;
-class Car {
-    private final String name;
-    private final int speed;
 
-    public Car(String name, int speed) {
-        this.name = name;
-        this.speed = speed;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    // Метод для расчёта пройденного расстояния за 24 часа
-    public int getDistanceIn24Hours() {
-        return speed * 24;
-    }
-}
-
-// Класс, отвечающий за логику гонки
-class Race {
-    private Car leader;
-
-    // Метод для определения лидера гонки
-    public void findLeader(Car[] cars) {
-        for (Car car : cars) {
-            if (leader == null || car.getDistanceIn24Hours() > leader.getDistanceIn24Hours()) {
-                leader = car;
-            }
-        }
-    }
-
-    public Car getLeader() {
-        return leader;
-    }
-}
-
-// Главный класс
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Car[] cars = new Car[3];
 
-        // Считывание данных об автомобилях
         for (int i = 0; i < cars.length; i++) {
             System.out.println("Введите данные для автомобиля " + (i + 1) + ":");
 
@@ -66,11 +27,9 @@ public class Main {
             cars[i] = new Car(name, speed);
         }
 
-        // Определение лидера
         Race race = new Race();
         race.findLeader(cars);
 
-        // Вывод лидера
         Car leader = race.getLeader();
         System.out.println("Самая быстрая машина: " + leader.getName());
 
